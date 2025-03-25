baaa5fee82c25cacbaf18a7d31106e1f1e660074
Omole kessiena David <edokadavid@gmail.com>
2025-03-25 12:41:25 +0100

    Update UserResource.php

--- app/Http/Resources/Admin/UserResource.php
+++ app/Http/Resources/Admin/UserResource.php
@@ -39,6 +39,10 @@ class UserResource extends JsonResource
                 'created_at' => $this->created_at,
                 'updated_at' => $this->updated_at,
             ],
+            'relationships' => [
+                'images' => $this->images,
+                'subscriptions' => $this->subscriptions
+            ]
         ];
     }
 }
