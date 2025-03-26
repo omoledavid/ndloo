c9b531e83a8ebf99c620f5cfc5626ebe5c5e3c7b
Etorojah Okon <etorojahokon100@gmail.com>
2025-03-26 16:10:35 +0100

    implemented instant gifting

--- app/Support/Services/GiftService.php
+++ app/Support/Services/GiftService.php
@@ -8,14 +8,15 @@ use App\Contracts\DataObjects\TransactionData;
 use App\Contracts\Enums\TransactionIcons;
 use App\Contracts\Enums\TransactionTypes;
 use App\Http\Resources\GiftPlanResource;
-use App\Jobs\GiftJob;
 use App\Models\GiftPlan;
 use App\Models\Transaction;
 use App\Models\User;
 use App\Models\UserGift;
-use App\Notifications\GiftRedeemedNotice;
+use App\Notifications\Gift\GiftReceivedNotice;
+use App\Notifications\Gift\GiftSentNotice;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 
@@ -37,9 +38,55 @@ class GiftService extends BaseService
 
     public function purchase(GiftPlan $giftPlan, User $recipient): JsonResponse
     {
-        GiftJob::dispatchAfterResponse($giftPlan, $recipient);
+        try {
+            Cache::lock(request()->user()->id, 10)->block(10, function () use ($giftPlan, $recipient) {
+                $sender                 =  User::where('id', request()->user()->id)->first();
+                $senderBalanceAfter     =  floatval(bcsub($sender->wallet, $giftPlan->amount, 2));
+                $recipientBalanceAfter  =  floatval(bcadd($recipient->wallet, $giftPlan->amount, 2));
 
-        return $this->successResponse(__('responses.giftProcessing'));
+                if ($senderBalanceAfter < 0) {
+                    throw new \Exception(__('responses.insufficientFunds'));
+                }
+
+                DB::transaction(function () use ($giftPlan, $sender, $recipient, $senderBalanceAfter, $recipientBalanceAfter) {
+                    $sender->update(['wallet' => $senderBalanceAfter]);
+                    $recipient->update(['credits' => $recipientBalanceAfter]);
+
+                    UserGift::create([
+                        'user_id'      => $recipient->id,
+                        'sender_id'    => $sender->id,
+                        'gift_plan_id' => $giftPlan->id,
+                    ]);
+
+                    Transaction::create(TransactionData::fromArray([
+                        'name'      => TransactionTypes::GIFT_SENT->value,
+                        'user_id'   => $sender->id,
+                        'amount'    => $giftPlan->amount,
+                        'icon'      => TransactionIcons::GIFT->value,
+                        'currency'  => 'USD',
+                        'usdAmount' => $giftPlan->amount,
+                    ])->toArray());
+
+                    Transaction::create(TransactionData::fromArray([
+                        'name'      => TransactionTypes::GIFT_RECEIVED->value,
+                        'user_id'   => $recipient->id,
+                        'amount'    => $giftPlan->amount,
+                        'icon'      => $giftPlan->icon,
+                        'currency'  => 'USD',
+                        'usdAmount' => $giftPlan->amount,
+                    ])->toArray());
+                });
+
+                $sender->notify(new GiftSentNotice($recipient, $giftPlan->amount));
+                $recipient->notify(new GiftReceivedNotice($sender, $giftPlan->amount));
+            });
+
+            return $this->successResponse(__('responses.giftSent'));
+        } catch (\Throwable $th) {
+            report($th);
+
+            return $this->errorResponse($th->getMessage());
+        }
     }
 
     public function redeem(UserGift $gift, Request $request): JsonResponse
@@ -73,7 +120,7 @@ class GiftService extends BaseService
             ])->delete();
 
             DB::commit();
-//            $request->user()->notify(new GiftRedeemedNotice($request->user(), $gift->plan->amount / 2));
+            //            $request->user()->notify(new GiftRedeemedNotice($request->user(), $gift->plan->amount / 2));
 
             return $this->successResponse(__('responses.giftRedeemed'));
         } catch (\Throwable $th) {
